d4cd4f407da4bc0c1f8791d3dd8628baa501b58b
BairamovAllaz <ellezbayramov2004@gmail.com>
2022-07-09 00:15:28 +0300

    Added Calc

---
+++ Calculator.App/Calc.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Markup;
+
+namespace Calculator.App;
+
+public static class Calc
+{
+    public static List<string> ParseString(string input)
+    {
+        char[] separators = {'-', '+','X','/'};
+        List<string> subs = input.Split(separators).ToList();
+        return subs;
+    }
+}
--- Calculator.App/MainWindow.xaml.cs
+++ Calculator.App/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Controls;
 
 namespace Calculator.App
 {
@@ -11,5 +15,30 @@ namespace Calculator.App
         {
             InitializeComponent();
         }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            Button ?button = sender as Button;
+            InputBox.Text += button?.Content.ToString();
+        }
+        private void ButtonC_OnClick(object sender, RoutedEventArgs e)
+        {
+            var text = InputBox.Text;
+            InputBox.Text = text.Remove(text.Length-1, 1);
+        }
+        private void ButtonClearAll_OnClick(object sender, RoutedEventArgs e)
+        {
+            InputBox.Text = "";
+        }
+        private void ButttonEqual_OnClick(object sender, RoutedEventArgs e)
+        {
+            List<string> l = Calc.ParseString(InputBox.Text);
+            for (int i = 0; i < l.Count; i++)
+            {
+                MessageBox.Show(l[i].ToString());
+            }
+        }
+        
+        
     }
 }
