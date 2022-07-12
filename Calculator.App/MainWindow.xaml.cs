3ae884dcd58c0ff4fe46bd546e3f384c56430234
BairamovAllaz <ellezbayramov2004@gmail.com>
2022-07-13 00:01:56 +0300

    adding

--- Calculator.App/MainWindow.xaml.cs
+++ Calculator.App/MainWindow.xaml.cs
@@ -16,6 +16,7 @@ namespace Calculator.App
         {
             calc = new Calc();
             InitializeComponent();
+            //InitCalc(4,4);
         }
 
         private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
@@ -62,6 +63,35 @@ namespace Calculator.App
                 }
                 var result = calc.GetResult();
                 InputBox.Text = result.ToString() + button.Content;
+            }
+        }
+
+        //TODO : Fix InitCalc
+        public void InitCalc(int Rows,int Columns)
+        {
+            for (int i = 0; i < Rows; i++)
+            {
+                ((Grid)CalcGrid).RowDefinitions.Add(new RowDefinition());
+            }
+            for (int i = 0; i < Columns; i++)
+            {
+                ((Grid)CalcGrid).ColumnDefinitions.Add(new ColumnDefinition());
+            }
+
+            var count = 0;
+            for (int i = 0; i < Rows; i++)
+            {
+                for (int j = 0; j < Columns; j++)
+                {
+                    var button = new Button() { Content = count };
+                    count++;
+                    Grid.SetRow(button,i);                       
+                    Grid.SetColumn(button,j);
+                    CalcGrid.Children.Add(button);
+                    button.Content = count.ToString();
+                    button.Style = (Style)Resources["Style_ButtonDefault"];
+                    button.Click += ButtonOperation_OnClick;
+                }
             }
         }
     }
